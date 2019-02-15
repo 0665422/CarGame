d15e7a1bc10d62b974af11f1941f8d44b94b84ef
7071 BusLaptop <7071-Buslaptop@dadeschools.net>
2019-02-15 14:15:59 -0500

    Loogic done

--- main.js
+++ main.js
@@ -1,34 +1,89 @@
 // VARIABLES
-let car = {
-    make: 'Lamborghini',
-    model: 'Veneno',
+const car = {
+    make: 'Tesla',
+    model: 'Roadster',
     color: 'red',
     mileage: 0,
-    isWorking: true;
+    isWorking: true,
 
-    driveToWork() {
+    driveToWork(){
         alert(`Old Mileage: ${this.mileage}`);
         this.mileage += 10;
         alert(`New Mileage: ${this.mileage}`);
     },
+
+    driveAroundUSA() {
+        alert(`Old Mileage: ${this.mileage}`);
+        this.mileage += 7000;
+        alert(`New Mileage: ${this.mileage}`);
+        alert(`Car needs a tuneup`);
+        this.isWorking = false;
+    },
+
+    getTuneup() {
+        this.isWorking = true;
+        alert('Car is fixed and ready to go!');
+    },
+
+    honk() {
+        alert('Honk! Honk!');
+    }
 };
 
-// FUNCTION 
-// Logs all of ur car's current stats to the console
-/*
-function reWriteStats() {
+// FUNCTIONS
+// Logs all of our car's current stats to the console
+
+reWriteStats = () => {
+    console.log(`Make: ${car.make}`);
+    console.log(`Model: ${car.model}`);
+    console.log(`Color: ${car.color}`);
+    console.log(`Mileage: ${car.mileage}`);
+    console.log(`Working?: ${car.isWorking}`);
+    console.log(`------------------------`);
+}
+
+
+// MAIN PROCESS
+// Captures keyboard input. Depending on the letter pressed it will "call" (execute) different functions.
+document.onkeyup = (event) => {
+    // Captures the key press, converts it to lowercase, and saves it to a variable
+    let letter = String.fromCharCode(event.keyCode).toLowerCase();
+
+    if (car.isWorking === true) {
+        if (letter === "t") {
+            car.getTuneup();
+            reWriteStats();
+        }
+    
+            if (letter === "h") {
+            car.honk();
+            reWriteStats();
+        }
+
+        if (letter === "d" && car.isWorking === true) {
+            car.driveToWork();
+            reWriteStats();
+        }
+
+        if (letter === "r") {
+            car.driveAroundUSA();
+            reWriteStats();
+        }
+    } else {
+        if (letter === "t") {
+            car.getTuneup();
+            reWriteStats();
+        }
+    
+        if (letter === "h") {
+            car.honk();
+            reWriteStats();
+        }
+
+        if (letter === "d" || letter ==="r") {
+            alert(`Can't drive. Fix you car, please.`);
+        }
+    }
+
     
 }
-*/
-
-//MAIN PROCESS
-// Captures keyboard input and runs functions
-document.onkeyup = (event) => {
-    // Captures the key, converts to lowercase and saves it to a variable
-    let letter = String.fromCharCode(event.keyCode).toLowerCase();
-    
-    if (letter === 'd') {
-        car.driveToWork();
-        reWriteStats();        
-    };
-};
